e631ef6f783fabe68aba974e6011e1443e6b7601
TheLis3k <ksawery2k19@gmail.com>
2026-06-20 14:54:33 +0200

    init

---
+++ src/main/java/pl/app/jaz32941nbp/Jaz32941nbpApplication.java
@@ -0,0 +1,13 @@
+package pl.app.jaz32941nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaz32941nbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jaz32941nbpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/app/jaz32941nbp/Jaz32941nbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.app.jaz32941nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaz32941nbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
